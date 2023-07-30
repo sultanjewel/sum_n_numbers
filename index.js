2d293862482dba32bb0da3174127c67d66d8a14a
Md. Sultanul Mulk <born2win.bd@gmail.com>
2023-07-30 11:37:17 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+
+//calculate sum of n natural numbers
+
+function sum_of_natural_numbers(n) {
+  //mathematic formula to calculate sum of n natural numbers= n(n+1)/2
+   retun n(n+1)/2; 
+}
+
+// Time complexity= O(1);
+// Space complexity- O(1);
